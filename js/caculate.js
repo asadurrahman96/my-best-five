c7f7078955b742cab2885838e33ec79f7aff7a32
asadurrahman96 <mdasadurrahmanchowdhury96@gmail.com>
2023-03-06 00:20:59 +0600

    calculate total added

--- js/caculate.js
+++ js/caculate.js
@@ -18,15 +18,55 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
     expensesTextElement.innerText = totalAmmount;
     budgetFieldElement.value = '';
 
-    document.getElementById('btn-total').addEventListener('click' , function(){
-        const managerAmmount = elementFieldId('manager-amount');
-        const coachAmmount = elementFieldId('coach-amount');
-        const expensesTextElementResult = inputTextElementId('player-expense');
-        const totalAmmount = managerAmmount + coachAmmount + expensesTextElementResult;
-        const finalAmmount = inputTextElementId('total-ammount');
-        const finalResult = totalAmmount + finalAmmount;
-        getElementById('total-ammount',finalResult);
-    })
+
+})
+
+
+
+
+    // document.getElementById('btn-total').addEventListener('click',function(){
+    //     const managerAmmount = document.getElementById('manager-amount');
+    //     const newManagerAmmountString = managerAmmount.value;
+    //     managerAmmount.value = '';
+    //     const newManagerAmmount = parseFloat(newManagerAmmountString);
+    //     const coachAmmount = document.getElementById('coach-amount');
+    //     const newCoachAmmountString = coachAmmount.value;
+    //     coachAmmount.value = '';
+    //     const newCoachAmmount = parseFloat(newCoachAmmountString);
+    
+    //     const totalManagerCoachAmmount = newManagerAmmount + newCoachAmmount;
+    //     console.log(totalManagerCoachAmmount);
+    //     const totalExpense = document.getElementById('total-ammount');
+    //     const newTotalExpenseString = totalExpense.innerText;
+    //     const newTotalExpense = parseFloat(newTotalExpenseString);
+        
+    
+    //     const newTotal = newTotalExpense + totalManagerCoachAmmount;
+        
+    //     totalExpense.innerText = newTotal;
+    
+    
+    
+    // })
+
+   document.getElementById('btn-total').addEventListener('click' ,function(){
+    
+    const managerAmmountField = document.getElementById('manager-amount');
+    const managerAmmount = parseFloat(managerAmmountField.value);
+    
+    const coachAmmountField = document.getElementById('coach-amount');
+    const coachAmmount = parseFloat(coachAmmountField.value);
+    
+    const playerExpenseTotal = document.getElementById('player-expense');
+    const playerExpense = parseFloat(playerExpenseTotal.innerText);
+    const totalCalculate = playerExpense + managerAmmount + coachAmmount;
+    const total = document.getElementById('total-ammount');
+    console.log('clicked');
+    total.innerText = totalCalculate;
+    // const previousTotal = total.innerText;
+    // previousTotal.innertext = totalCalculate;
+
+   })
     
 
     
@@ -40,32 +80,8 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
   
     
 
-})
-
-document.getElementById('btn-total').addEventListener('click',function(){
-    const managerAmmount = document.getElementById('manager-amount');
-    const newManagerAmmountString = managerAmmount.value;
-    managerAmmount.value = '';
-    const newManagerAmmount = parseFloat(newManagerAmmountString);
-    const coachAmmount = document.getElementById('coach-amount');
-    const newCoachAmmountString = coachAmmount.value;
-    coachAmmount.value = '';
-    const newCoachAmmount = parseFloat(newCoachAmmountString);
-
-    const totalManagerCoachAmmount = newManagerAmmount + newCoachAmmount;
-    
-    const totalExpense = document.getElementById('total-ammount');
-    const newTotalExpenseString = totalExpense.innerText;
-    const newTotalExpense = parseFloat(newTotalExpenseString);
-    
-
-    const newTotal = newTotalExpense + totalManagerCoachAmmount;
-    
-    totalExpense.innerText = newTotal;
 
 
-
-})
 
 // document.getElementById('btn-total').addEventListener('click', function(){
 //     const calculateTotal = document.getElementById('total-ammount');
